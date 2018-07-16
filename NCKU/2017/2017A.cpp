8cf1969aa2afbcfafb21cd694e459979ac206043
Python <38965858+larrychen20011120@users.noreply.github.com>
2018-07-16 20:50:37 +0800

    Add files via upload

---
+++ NCKU/2017/2017A.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main(int argc, const char * argv[]) {
+	int n;
+	cin>>n;
+	string a;
+	char cont[7]={'N','C','K','U','S','I','E'};
+	for(int i=1;i<=n;i++){
+		cin.ignore();
+		getline(cin,a);
+		int b[7]={0,0,0,0,0,0,0};
+		for(int k=0;k<a.length();k++){
+			for(int j=0;j<7;j++){
+				if(a[k]==cont[j]){
+					b[j]++;
+				}
+			}
+		}
+		b[1]=b[1]/2;
+		for(int k=0;k<6;k++){
+				if(b[k+1]>b[k]){
+					b[k+1]^=b[k]^=b[k+1]^=b[k];
+			}
+		}
+		cout<<b[6]<<endl;
+	}
+    return 0;
+}
+
